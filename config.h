60f4c835705de6f5b920343925558c7058ea4411
ericlarosa <eric@rosa.la>
2021-09-07 11:47:33 -0300

    :shirt:

--- config.h
+++ config.h
@@ -40,8 +40,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 
-
-
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
     #define RGBLIGHT_ANIMATIONS
