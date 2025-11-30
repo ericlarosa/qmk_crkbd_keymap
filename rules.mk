432eac6605c97d47e20efaf17acac4208404db9f
ericlarosa <ericlarosa95@gmail.com>
2025-11-30 16:32:52 -0300

    changes for rev4

--- rules.mk
+++ rules.mk
@@ -1,2 +1,4 @@
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
-OLED_DRIVER_ENABLE  = no
+RGBLIGHT_ENABLE = no       # Disable WS2812 RGB underlight
+RGB_MATRIX_ENABLE = no     # Disable RGB matrix
+OLED_DRIVER_ENABLE = no    # Disable OLED displays
+BACKLIGHT_ENABLE = no      # Disable keyboard backlight
